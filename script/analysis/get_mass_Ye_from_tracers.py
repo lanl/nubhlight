bfb8b3fd0d4525b94d659ab1821fea9503ac87f8
Jonah Miller <jonahm@lanl.gov>
2020-12-15 18:32:49 -0700

    get mass and Ye from tracers

--- script/analysis/get_mass_Ye_from_tracers.py
+++ script/analysis/get_mass_Ye_from_tracers.py
@@ -51,19 +51,21 @@ def get_intersection(tracers,tracers_ref):
 
 class OutflowProperties:
     "Container class containing outflow mass and Ye"
-    def __init__(self, tracers):
+    def __init__(self, tracers, m_acc):
         from units import cgs
         # Mass in solar masses
         mass = tracers['mass'].sum()
         code2msolar = tracers.units['M_unit']/cgs['MSOLAR']
         self.mass = mass*code2msolar
+        # mass divided by total mass accreted
+        self.frac_mass = self.mass / m_acc
         # Mass-averaged Ye
         self.Ye = (tracers['mass']*tracers['Ye']).sum()/mass
 
     def __str__(self):
-        return "{:.5e} {:.5e}".format(self.mass, self.Ye)
+        return "{:.5e} {:.5e} {:.5e}".format(self.mass, self.frac_mass, self.Ye)
 
-def make_table(MBH, a, Md, Ye, s, sphere_files, nse_files):
+def make_table(MBH, a, Md, Ye, s, m_acc, sphere_files, nse_files):
     """Make formatted (ascii) table and return it as a string
     for each simulation with:
     - Black hole mass MBH in solar masses
@@ -71,15 +73,15 @@ def make_table(MBH, a, Md, Ye, s, sphere_files, nse_files):
     - Disk mass Md in solar masses
     - Electron fraction Ye (unitless)
     - Entropy s in K_b/baryon
-    - a tracerdata file ending in *.td
+    - m_acc is the amount of mass accreted by the end of the simulation
+    - sphere_files are files of tracers extracted at a radius of 250 rg
+    - nse_files are files of tracers extracted when they drop below 5GK
     Note some additional parameters that would be good to add:
     - Magnetic field strength
     - Magnetic field topology
-    sphere_files are files of tracers extracted at a radius of 250 rg
-    nse_files are files of tracers extracted when they drop below 5GK
     """
     from hdf5_to_dict import TracerData
-    assert len(sphere_files) == len(nse_files)
+    assert len(sphere_files) == len(nse_files) == len(m_acc)
     assert len(MBH) == len(a) == len(Md) == len(Ye) == len(s) == len(nse_files)
 
     out = """# Columns:
@@ -89,11 +91,17 @@ def make_table(MBH, a, Md, Ye, s, sphere_files, nse_files):
 # [3]:  Disk Ye at the initial time (unitless)
 # [4]:  Disk entropy at initial time (k_b/baryon)
 # [5]:  Total mass in outflow (solar masses)
-# [6]:  Mass-averaged Ye in total outflow (unitless)
-# [7]:  Mass in polar region, >= 50 degress off equator (solar masses)
-# [8]:  Mass-averaged Ye in polar region
-# [9]:  Mass in equatorial region, <= 15 degrees off equator (solar masses)
-# [10]: Mass-averaged Ye in equatorial region
+# [6]:  Ratio of total mass in outflow to mass accreted
+# [7]:  Mass-averaged Ye in total outflow (unitless)
+# [8]:  Extrapolated total mass in outflow to late times
+# [9]:  Mass in polar region, >= 50 degress off equator (solar masses)
+# [10]: Ratio of mass in polar region to mass accreted
+# [11]: Mass-averaged Ye in polar region
+# [12]: Extrapolated mass in polar region
+# [13]: Mass in equatorial region, <= 15 degrees off equator (solar masses)
+# [14]: Ratio of mass in equatorial region to mass accreted
+# [15]: Mass-averaged Ye in equatorial region
+# [16]: Extrapolated mass in equatorial region
 # Note that polar + equatorial != total!
 """
     for i,(f_sph,f_nse) in enumerate(zip(sphere_files,nse_files)):
@@ -112,16 +120,20 @@ def make_table(MBH, a, Md, Ye, s, sphere_files, nse_files):
         polar_nse = get_intersection(tracers_nse, polar)
 
         # Get outflow properties
-        prop_tot = OutflowProperties(tracers_nse)
-        prop_pol = OutflowProperties(polar_nse)
-        prop_equ = OutflowProperties(equatorial_nse)
+        prop_tot = OutflowProperties(tracers_nse,m_acc[i])
+        prop_pol = OutflowProperties(polar_nse,m_acc[i])
+        prop_equ = OutflowProperties(equatorial_nse,m_acc[i])
         
-        out += "{:.3e} {:.3e} {:.3e} {:.2e} {:.2e} {} {} {}\n".format(MBH[i],a[i],
-                                                                      Md[i],Ye[i],
-                                                                      s[i],
-                                                                      prop_tot,
-                                                                      prop_pol,
-                                                                      prop_equ)
+        out += "{:.3e} {:.3e} {:.3e} {:.2e} {:.2e} {} {:.3e} {} {:.3e} {} {:.3e}\n".format(
+            MBH[i],a[i],
+            Md[i],Ye[i],
+            s[i],
+            prop_tot,
+            prop_tot.frac_mass*Md[i],
+            prop_pol,
+            prop_pol.frac_mass*Md[i],
+            prop_equ,
+            prop_equ.frac_mass*Md[i])
 
     return out
                                                                     
@@ -140,10 +152,13 @@ if __name__ == "__main__":
                         help="initial Ye for each simulation (unitless)")
     parser.add_argument("-s","--entropy",type=float,nargs='+',required=True,
                         help="initial entropy for each simulation (k_b/baryon)")
+    parser.add_argument("--macc",type=float,nargs='+',required=True,
+                        help="mass accreted by end of simulation (msolar)")
     parser.add_argument("--tracers",type=str,nargs="+",required=True,
                         help="files with tracer data when tracers pass through extraction sphere")
     parser.add_argument("--nse",type=str,nargs="+",required=True,
                         help="files with tracer data when tracers fall below 5GK")
     args = parser.parse_args()
-    table = make_table(args.mbh,args.spin,args.mdisk,args.ye,args.entropy,args.tracers,args.nse)
+    table = make_table(args.mbh,args.spin,args.mdisk,args.ye,args.entropy,
+                       args.macc,args.tracers,args.nse)
     print(table)
