a6a4aae295937d166e195c5e1cf52f294c993af9
Jonah Miller <jonahm@lanl.gov>
2024-12-19 18:46:43 -0700

    modify oscillation time scale formula

--- core/constants.h
+++ core/constants.h
@@ -30,7 +30,7 @@
 
 // Frequency scale of neutrino oscillations
 #define ROOT2 (1.4142135623730951)
-#define NUFERM (ROOT2*HBAR*HBAR*CL*CL*CL*GFERM)
+#define NUFERM (HBAR / (ROOT2*GFERM))
 
 // Unit conversions
 #define EV (1.60217653e-12)   // Electron-volt
